72f33f6553981f2cf93327c4b23b02234a52e5d7
mcinkling <47238646+mcinkling@users.noreply.github.com>
2019-11-11 22:51:38 +0100

    Add Features to main.cpp
    Allow setting the output detail (default: 0 (pi only)) 
    Add a default value for cout.precision (25)
    Allow setting the for cycles/sides on the almost-circle (default: 100 000 sides)
    
    Add some comments
    
    Remove unnecessary variable pi as it's just u/2
    Make the output look prettier

--- main.cpp
+++ main.cpp
@@ -1,49 +1,71 @@
 #include <iostream>
+#include <sstream>
 #include <cmath>
 using namespace std;
 
 int main() {
+	// Set the initial values
 	long double a = 1;
 	long double u = 6;
-	long double pi = 3;
 	long double h = sqrt(0.75);
 	long double x = 1-h;
 
-	/*bool simple;
-	cout << "Simplified Output? (0 - 1): ";
-	cin >> simple;*/
+	string input;
 
-	int coutp;
-	cout << "Output Length (1 - 75): ";
-	cin >> coutp;
+	// Boolean that stops unnecessary variables from being printed later
+	bool fulloutp = 0;
+	cout << "Full Output? (0 - 1) [default: 0]: ";
+	getline(cin, input);
+	if (!input.empty()) {
+		istringstream stream(input);
+		stream >> fulloutp;
+	}
+
+	// Set the amount of digits after the decimal point
+	int coutp = 25;
+	cout << "Output Length (1 - 75) [default: 25]: ";
+	getline(cin, input);
+	if (!input.empty()) {
+		istringstream stream(input);
+		stream >> coutp;
+	}
 	cout.precision(coutp);
 
-	/*int cycles;
-	cout << "Precision (): ";
-	cin >> cycles;*/
+	// Set how often the following for loop will be repeated	longer = more accurate
+	long long sides = 100000;
+	cout << "Amount of sides on the circle (6 - inf) [default: 100000]: ";
+	getline(cin, input);
+	if (!input.empty()) {
+		istringstream stream(input);
+		stream >> sides;
+	}
 
-	cout << endl << \
-	"n =  " << 6 << endl << \
-	"a" << 6 << " =  " << a << endl << \
-	"u" << 6 << " =  " << u << endl << \
-	"h" << 6 << " =  " << h << endl << \
-	"x" << 6 << " =  " << x << endl << \
-	"PI = " << pi << endl;
+	// Output the values for hexagon
+	if (fulloutp == true) {
+		cout << endl << \
+		"a" << 6 << "  =  " << a << endl << \
+		"u" << 6 << "  =  " << u << endl << \
+		"h" << 6 << "  =  " << h << endl << \
+		"x" << 6 << "  =  " << x << endl;
+	}
+	cout << "pi6 =  " << 3 << endl;
 
 
-	for (long long n = 12; n < 1000000; n*=2) {
+	// Increase the amount of sides in order to get closer to a circle
+	for (long long n = 12; n <= sides; n*=2) {
 		a = sqrt((0.5*a)*(0.5*a)+x*x);
 		u = n*a;
-		pi = u/2;
 		h = sqrt(1-(0.5*a)*(0.5*a));
 		x = 1-h;
 
-		cout << endl << \
-		"n =  " << n << endl << \
-		"a" << n << " =  " << a << endl << \
-		"u" << n << " =  " << u << endl << \
-		"h" << n << " =  " << h << endl << \
-		"x" << n << " =  " << x << endl << \
-		"PI = " << pi << endl;
+		if (fulloutp == true) {
+			// Output the values used for the calculation, if Full Output was enabled
+			cout << endl << \
+			"a" << n << "  =  " << a << endl << \
+			"u" << n << "  =  " << u << endl << \
+			"h" << n << "  =  " << h << endl << \
+			"x" << n << "  =  " << x << endl;
+		}
+		cout << "pi" << n << " =  " << u/2 << endl;
 	}
 }
